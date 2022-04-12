8834d0c7420be74a1fc5ac4d9f326702ab28fbeb
Nahiyan Bin Noor <90473181+Nahiyan140212@users.noreply.github.com>
2022-04-12 14:01:32 -0500

    Create Two Sum.py

---
+++ Blind 75/Easy/Two Sum.py
@@ -0,0 +1,28 @@
+#Brute Force
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i,j]
+  TC: O(n*n)
+  SC: O(1)
+    
+    
+#Using Hashmap:
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap =  {}
+        
+        for i in range(len(nums)):
+            k = target - nums[i]
+            if k in hashmap:
+                return [i, hashmap[k]]
+            else:
+                hashmap[nums[i]] = i
+                
+                
+  TC: O(n)
+  SC: O(1)
