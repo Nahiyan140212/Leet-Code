5fcbd940d5487f4bf93e6324045e50a1ae71e625
Nahiyan Bin Noor <90473181+Nahiyan140212@users.noreply.github.com>
2022-04-15 22:23:15 -0500

    Create Apr_15.py

---
+++ April_2022/Apr_15.py
@@ -0,0 +1,38 @@
+
+#538. Convert BST to Greater Tree
+#Using Stack
+
+class Solution(object):
+    def convertBST(self, root):
+        total = 0
+        
+        node = root
+        stack = []
+        while stack or node is not None:
+            # push all nodes up to (and including) this subtree's maximum on
+            # the stack.
+            while node is not None:
+                stack.append(node)
+                node = node.right
+
+            node = stack.pop()
+            total += node.val
+            node.val = total
+
+            # all nodes with values between the current and its parent lie in
+            # the left subtree.
+            node = node.left
+
+        return root
+      
+#Recursion
+class Solution:
+    def __init__(self):
+        self.total = 0
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
